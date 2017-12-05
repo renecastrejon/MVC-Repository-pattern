f4f1ff41375816760d696455df83413b1bf8a237
Balluff <Balluff@MXBN0190>
2017-12-05 15:45:10 -0600

    Automapper added

---
+++ MyProject.BLL/Service/UserServiceRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProject.IBLL;
+using MyProject.Model;
+using MyProject.Model.ViewModels;
+using MyProject.IBLL.Service;
+
+namespace MyProject.BLL.Service
+{
+    public class UserServiceRepository : IUserService
+    {
+        private readonly IRepository<User> _useRepository;
+
+        public UserServiceRepository(IRepository<User> userRepository)
+        {
+            _useRepository = userRepository;
+        }
+        public IEnumerable<UserViewModel> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserViewModel GetBy(Guid userid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyProject.IBLL/Service/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProject.Model.ViewModels;
+
+
+namespace MyProject.IBLL.Service
+{
+    public interface IUserService
+    {
+        IEnumerable<UserViewModel> GetAllUsers();
+        UserViewModel GetBy(Guid userid);
+    }
+}
